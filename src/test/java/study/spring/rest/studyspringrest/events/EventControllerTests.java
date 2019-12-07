3c6ecfd52a20c65ec04884a5e4b0b58cac65644c
김윤수 <younsookim@tmon.co.kr>
2019-12-08 02:13:18 +0900

    201 응답 받기

--- src/test/java/study/spring/rest/studyspringrest/events/EventControllerTests.java
+++ src/test/java/study/spring/rest/studyspringrest/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package study.spring.rest.studyspringrest.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -19,11 +25,30 @@ public class EventControllerTests {
 	@Autowired
 	MockMvc mockMvc;
 
+	@Autowired
+	ObjectMapper objectMapper;
+
 	@Test
 	public void createEvent() throws Exception {
+		Event event = Event.builder()
+				.name("Spring")
+				.description("REST API Development with Spring")
+				.beginEnrollmentDateTime(LocalDateTime.of(2019, 12, 8, 2, 2))
+				.closeEnrollmentDateTime(LocalDateTime.of(2019, 12, 9, 2, 2))
+				.beginEventDateTime(LocalDateTime.of(2019, 12, 13, 2, 2))
+				.endEventDateTime(LocalDateTime.of(2019, 12, 14, 2, 2))
+				.basePrice(100)
+				.maxPrice(200)
+				.limitOfEnrollment(100)
+				.location("강남역 D2 스타텁 팩토리")
+				.build();
+
 		mockMvc.perform(post("/api/events/")
 					.contentType(MediaType.APPLICATION_JSON)
-					.accept(MediaTypes.HAL_JSON))
-				.andExpect(status().isCreated()); // == status().is(201)
+					.accept(MediaTypes.HAL_JSON)
+					.content(objectMapper.writeValueAsString(event)))
+				.andDo(print())
+				.andExpect(status().isCreated()) // == status().is(201)
+				.andExpect(jsonPath("id").exists());
 	}
  }
