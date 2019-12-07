de61e204b3c6200674b9675ec571dc8e83e6dc55
김윤수 <younsookim@tmon.co.kr>
2019-12-07 23:32:38 +0900

    Project 만들기

---
+++ src/main/java/study/spring/rest/studyspringrest/StudySpringRestApplication.java
@@ -0,0 +1,13 @@
+package study.spring.rest.studyspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringRestApplication.class, args);
+	}
+
+}
